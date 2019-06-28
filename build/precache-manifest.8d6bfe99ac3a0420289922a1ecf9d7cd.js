029c22dd67a9c62e1df35f99327bacba6baaebd3
Tara Patel <t.jeanne.patel@gmail.com>
2019-06-28 12:02:50 -0500

    added build for gh pages

---
+++ build/precache-manifest.8d6bfe99ac3a0420289922a1ecf9d7cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f1e904d2b51ba53cddc",
+    "url": "/static/css/main.62f46bef.chunk.css"
+  },
+  {
+    "revision": "4f1e904d2b51ba53cddc",
+    "url": "/static/js/main.4f1e904d.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab991172a8089bb618a34192b8121dc4",
+    "url": "/index.html"
+  }
+];
